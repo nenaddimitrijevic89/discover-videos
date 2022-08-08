8e5a13f9cd0f50b5901f581e3cbcf0de55b594d4
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-08-08 22:53:14 +0200

    Add image error handling

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,8 +3,11 @@ import Image from 'next/image'
 
 import styles from './Card.module.css'
 
+const placeholderImg =
+   'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80'
+
 const Card = (props) => {
-   const { imgUrl = '/static/clifford.jpg', size = 'medium' } = props
+   const { imgUrl = placeholderImg, size = 'medium' } = props
 
    const [imgSrc, setImgSrc] = useState(imgUrl)
 
@@ -15,7 +18,7 @@ const Card = (props) => {
    }
 
    const handleOnError = () => {
-      setImgSrc('/static/clifford.jpg')
+      setImgSrc(placeholderImg)
    }
 
    return (
