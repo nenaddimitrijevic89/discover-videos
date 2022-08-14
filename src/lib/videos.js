d04ad2e0094d78b51a5fdf03f88f8578a8aae51a
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-08-14 19:42:21 +0200

    Add youtube images in config

--- src/lib/videos.js
+++ src/lib/videos.js
@@ -1,5 +1,9 @@
 import videosData from 'data/videos'
 
 export const getVideos = () => {
-   return videosData.items
+   return videosData.items.map((item) => ({
+      title: item.snippet.title,
+      imgUrl: item.snippet.thumbnails.high.url,
+      id: item?.id?.videoId,
+   }))
 }
