fe8cadb7a17b3e17d3d21d09e095e509a9587ca0
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-09-01 22:56:29 +0200

    Refactor login serverless

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
          try {
             const { email } = await magic.user.getMetadata()
             const didToken = await magic.user.getIdToken()
-            console.log({ didToken })
+            // console.log({ didToken })
             if (email) {
                setUsername(email)
             }
--- src/lib/db/hasura.js
+++ src/lib/db/hasura.js
@@ -3,11 +3,27 @@ This is an example snippet - you should consider tailoring it
 to your service.
 */
 
-async function queryHasuraGQL(operationsDoc, operationName, variables) {
+export async function isNewUser(token, issuer) {
+   const operationsDoc = `
+      query isNewUser($issuer: String!) {
+         users(where: {issuer: {_eq: $issuer}}) {
+            email
+            id
+            issuer
+         }
+      `
+
+   const response = await queryHasuraGQL(operationsDoc, 'isNewUser', { issuer }, token)
+   console.log({ response, issuer })
+   return response?.data?.users?.length === 0
+}
+
+async function queryHasuraGQL(operationsDoc, operationName, variables, token) {
    const result = await fetch(process.env.NEXT_PUBLIC_HASURA_ADMIN_URL, {
       method: 'POST',
       headers: {
-         'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET,
+         Authorization: `Bearer ${token}`,
+         'Content-type': 'application/json',
       },
       body: JSON.stringify({
          query: operationsDoc,
@@ -17,31 +33,4 @@ async function queryHasuraGQL(operationsDoc, operationName, variables) {
    })
 
    return await result.json()
-}
-
-const operationsDoc = `
-    query MyQuery {
-      users {
-        id
-        issuer
-        publicAddress
-        email
-      }
-    }
-  `
-
-function fetchMyQuery() {
-   return queryHasuraGQL(operationsDoc, 'MyQuery', {})
-}
-
-export async function startFetchMyQuery() {
-   const { errors, data } = await fetchMyQuery()
-
-   if (errors) {
-      // handle those errors like a pro
-      console.error(errors)
-   }
-
-   // do something great with this precious data
-   console.log(data)
 }
