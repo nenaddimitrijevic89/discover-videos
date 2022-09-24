4ee2cb0d4f92a5d3dc820c7ee38e9e900f4f5205
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-09-25 00:43:08 +0200

    Add update stats graphql mutation

--- src/pages/api/stats.js
+++ src/pages/api/stats.js
@@ -1,24 +1,34 @@
 import jwt from 'jsonwebtoken'
 
-import { findVideoIdByUser } from 'lib/db/hasura'
+import { findVideoIdByUserId, updateStats } from 'lib/db/hasura'
 
 export default async function stats(req, res) {
    if (req.method === 'POST') {
-      console.log({ cookies: req.cookies })
-
       try {
          const token = req.cookies.token
          if (!token) {
             res.status(403).send({})
          } else {
-            const decoded = jwt.verify(token, process.env.HASURA_JWT_SECRET_KEY)
-            console.log({ decoded })
+            const decodedToken = jwt.verify(token, process.env.HASURA_JWT_SECRET_KEY)
 
-            const userId = 'did:ethr:0xA917b22eF8FE0E4ccB858ee19BDbe145e5048d29'
-            const videoId = '4zH5iYM4wJo'
+            const videoId = req.query.videoId
+            const userId = decodedToken.issuer
 
-            const findVideoId = await findVideoIdByUser(token, userId, videoId)
-            res.send({ msg: 'it works', decoded, findVideoId })
+            const doesStatsExist = await findVideoIdByUserId(token, userId, videoId)
+            if (doesStatsExist) {
+               // update it
+               const response = await updateStats(token, {
+                  userId,
+                  videoId,
+                  watched: false,
+                  favourited: 55,
+               })
+               console.log({ response })
+               res.send({ msg: 'it works', decodedToken, doesStatsExist })
+            } else {
+               // add it
+               res.send({ msg: 'it works', decodedToken, doesStatsExist })
+            }
          }
       } catch (err) {
          console.error('Error ocured /stats', err)
