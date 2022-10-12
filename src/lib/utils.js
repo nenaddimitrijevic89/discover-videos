f2f930122deccc7b18b9802d3b06aa58bea8f93e
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-10-12 23:10:49 +0200

    Implement middleware

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,11 +1,17 @@
-import jwt from 'jsonwebtoken'
+import { jwtVerify } from 'jose'
 
 export async function verifyToken(token) {
-   if (token) {
-      const decodedToken = jwt.verify(token, process.env.HASURA_JWT_SECRET_KEY)
-      const userId = decodedToken?.issuer
-
-      return userId
+   try {
+      if (token) {
+         const verified = await jwtVerify(
+            token,
+            new TextEncoder().encode(process.env.HASURA_JWT_SECRET_KEY)
+         )
+         return verified.payload && verified.payload?.issuer
+      }
+      return null
+   } catch (err) {
+      console.error({ err })
+      return null
    }
-   return null
 }
