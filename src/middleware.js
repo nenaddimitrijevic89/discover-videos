76fe78c65bcbf8b4420e1b65064dcc32235e2fc1
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-10-13 21:30:13 +0200

    Fix middleware

--- src/middleware.js
+++ src/middleware.js
@@ -3,19 +3,17 @@ import { NextResponse } from 'next/server'
 import { verifyToken } from 'lib/utils'
 
 export async function middleware(req, ev) {
-   const token = req ? req.cookies?.token : null
+   const token = req ? req.cookies.get('token') : null
    const userId = await verifyToken(token)
    const { pathname } = req.nextUrl
 
    if (pathname.includes('/api/login') || userId || pathname.includes('/static')) {
-      console.log('first')
       return NextResponse.next()
    }
 
    if ((!token || !userId) && pathname !== '/login') {
-      console.log('second')
       const url = req.nextUrl.clone()
       url.pathname = '/login'
-      // return NextResponse.rewrite(url)
+      return NextResponse.rewrite(url)
    }
 }
